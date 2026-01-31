da4db8c53b5adffcf7bc4fa958f437a0c91580e2
Rahul Gundapaneni <rahulgundapaneni@Mac.lan>
2026-01-31 12:58:24 -0600

    changes

--- 143 Reorder List.java
+++ 143 Reorder List.java
@@ -20,3 +20,54 @@ class Solution {
         head = result;
     }
 }
+
+// Method 2: Finding middle, reversing second half and merging both halves
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode test = head;
+        if (head == null || head.next == null) return;
+        ListNode slow = test;
+        ListNode fast = test;
+
+
+        while(fast.next != null && fast.next.next != null) 
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(fast.val);
+
+        // Now reverse the list
+        ListNode curr = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+        while(curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        curr = prev;
+        while(curr != null) {
+            ListNode temp = test.next;
+            ListNode currNext = curr.next;
+            test.next = curr;
+            curr.next = temp;
+            test = temp;
+            curr = currNext;
+        }
+    }
+}
+
